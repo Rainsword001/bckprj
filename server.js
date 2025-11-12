7bf6811bc14f6acb1521789d36b30b02f435d594
Rainsword001 <olowoyoemmanuel12@gmail.com>
2025-11-12 12:18:54 +0000

    Fix CORS origin configuration to allow multiple origins for better flexibility

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json()) // it parse out info to be display
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000" || "http://localhost:5000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
